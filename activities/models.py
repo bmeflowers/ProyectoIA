f2b0df3f2dcfc18f7c9c64678ff6aa441cbc87de
bmeflowers <142183800+bmeflowers@users.noreply.github.com>
2025-06-09 11:09:15 -0500

    nueva app y sus modelos

--- activities/models.py
+++ activities/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Actividad(models.Model):
+    TIPO_CHOICES = [
+        ('tarea', 'Tarea'),
+        ('habito', 'Hábito'),
+    ]
+    ESTADO_CHOICES = [
+        ('pendiente', 'Pendiente'),
+        ('completado', 'Completado'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con User
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    fecha_limite = models.DateField(null=True, blank=True)  # Solo para tareas
+    hora_habito = models.TimeField(null=True, blank=True)   # Solo para hábitos
+    estado = models.CharField(max_length=10, choices=ESTADO_CHOICES, default='pendiente')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+class RegistroHabito(models.Model):
+    habito = models.ForeignKey(Actividad, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    estado = models.CharField(max_length=10, choices=Actividad.ESTADO_CHOICES)
+
+    class Meta:
+        unique_together = ('habito', 'fecha')
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,21 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from Users import views
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('logout/', views.signout, name='logout'),
-    path('login/', views.signin, name='login'),
-    path('profile/', views.profile_view, name='profile'),
-    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('', include('Users.urls')),  #URLs de la app Users
 ]
 
 if settings.DEBUG:
